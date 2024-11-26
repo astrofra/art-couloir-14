cb55c68d00a9620a1ee403dde299f1a4c7bb81eb
fra <astrofra@gmail.com>
2024-11-26 21:04:16 +0100

    Reinstate the CRT output.

--- app/main.lua
+++ app/main.lua
@@ -34,7 +34,7 @@ hg.WindowSystemInit()
 hg.OpenALInit()
 
 SLIDE_SHOW_SPEED = 1.0
-VR_DEBUG_DISPLAY = true
+VR_DEBUG_DISPLAY = false
 -- local res_x, res_y = 768, 576
 -- local res_x, res_y = 800, 600
 local res_x, res_y = 960, 720
@@ -104,7 +104,7 @@ local text_uniform_values = {hg.MakeUniformSetValue('u_color', hg.Vec4(1, 1, 0, 
 local text_render_state = hg.ComputeRenderState(hg.BM_Alpha, hg.DT_Always, hg.FC_Disabled)
 
 -- VHS fx shader
-screen_prg = hg.LoadProgramFromAssets('shaders/vhs_fx.vsb', 'shaders/vhs_fx.fsb')
+local screen_prg = hg.LoadProgramFromAssets('shaders/vhs_fx.vsb', 'shaders/vhs_fx.fsb')
 
 -- create a plane model for the final rendering stage
 local vtx_layout = hg.VertexLayoutPosFloatNormUInt8TexCoord0UInt8()
@@ -338,6 +338,43 @@ while not keyboard:Pressed(hg.K_Escape) and hg.IsWindowOpen(win) do
 	end
 
 	view_id = view_id + 1
+
+	-- CRT display rendering
+	if open_vr_enabled then
+
+		local chroma_distortion = clamp(map(photo_state.noise_intensity, 0.1, 0.5, 0.0, 1.0), 0.0, 1.0)
+		local val_uniforms = {hg.MakeUniformSetValue('uControl', hg.Vec4(photo_state.noise_intensity, chroma_distortion, 0.0, 0.0))}
+		-- val_uniforms = {hg.MakeUniformSetValue('control', hg.Vec4(1.0, 1.0, 0.0, 0.0))} -- test only
+		-- _, tex_video, size, fmt = hg.UpdateTexture(streamer, handle, tex_video, size, fmt)
+	
+		local tex_uniforms = {
+			hg.MakeUniformSetTexture('u_video', video_fx_texture, 0),
+			hg.MakeUniformSetTexture('u_photo0', photo_state.tex_photo0.texture, 1)
+		}
+
+		hg.SetViewPerspective(view_id, 0, 0, res_x, res_y, hg.TranslationMat4(hg.Vec3(0, 0, -0.68 * zoom_level)))
+
+		hg.DrawModel(view_id, screen_mdl, screen_prg, val_uniforms, tex_uniforms, hg.TransformationMat4(hg.Vec3(0, 0, 0), hg.Vec3(math.pi / 2, math.pi, 0)))
+
+		-- text OSD
+		local osd_text = folder_short[photo_state.current_folder] .. (photo_state.current_photo - 1)
+		-- osd_text = ghostWorldAssociations[photo_state.index_photo0]
+		view_id = view_id + 1
+
+		hg.SetView2D(view_id, 0, 0, res_x, res_y, -1, 1, hg.CF_None, hg.Color.Black, 1, 0)
+
+		local text_pos = hg.Vec3(res_x * 0.05, res_y * 0.05, -0.5)
+		local _osd_colors = {hg.Vec4(1.0, 0.0, 0.0, 0.8), hg.Vec4(0.0, 1.0, 0.0, 0.8), hg.Vec4(1.0, 1.0, 1.0, 1.0)}
+		local _osd_offsets = {-2.0, 1.0, 0.0}
+
+		for _text_loop = 1, 3 do
+			local _text_offset = hg.Vec3(res_x * 0.001 * _osd_offsets[_text_loop] * photo_state.noise_intensity, 0.0, 0.0)
+			hg.DrawText(view_id, font_osd, osd_text, font_program, 'u_tex', 0, 
+					hg.Mat4.Identity, text_pos + _text_offset, hg.DTHA_Left, hg.DTVA_Bottom, 
+					{hg.MakeUniformSetValue('u_color', _osd_colors[_text_loop])}, 
+					{}, text_render_state)
+		end
+	end
 
 	-- Display the VR eyes texture to the backbuffer
 	if VR_DEBUG_DISPLAY and open_vr_enabled then
